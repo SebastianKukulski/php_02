15e534667ffa04b8accbcabcd51c8cd0f5e7e492
dt19kukulskis <skukulski1@gmail.com>
2022-09-28 09:31:43 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$title="Strona Główna";
+echo $title;
+
+?>
+<ol style="list-style: decimal-leading-zero">
+    <li><a href="plan_lekcji.php">Plan lekcji</a></li>
+    <li><a href="syntax.php">Funkcje</a></li>
+    <li><a href="variables.php">Zmienne</a></li>
+</ol>
+</body>
+</html>
