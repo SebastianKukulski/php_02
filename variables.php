1936cd1502bb53a36fb47ee12529964447bd2621
dt19kukulskis <skukulski1@gmail.com>
2022-09-28 09:35:36 +0200

    Add variables.php

---
+++ variables.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$string="Strycharczyk";
+echo "<br><br>$string<br>";
+var_dump($string);
+
+$liczba=18;
+echo "<br><br>$liczba<br>";
+var_dump($liczba);
+
+$float=18.5;
+echo "<br><br>$float<br>";
+var_dump($float);
+
+$bool=true;
+echo "<br><br>$bool<br>";
+var_dump($bool);
+?>
+
+<li><a href="index.php">Powrót do strony głównej</a></li>
+
+</body>
+</html>
