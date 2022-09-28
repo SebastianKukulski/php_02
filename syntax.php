4b536baebf378b73c82b8471e2cb6071a6c04a6a
dt19kukulskis <skukulski1@gmail.com>
2022-09-28 09:34:41 +0200

    Add syntax

---
+++ syntax.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$title="echo";
+echo "Tu". " funkcja". " echo<br>";
+echo "Tu funkcja ". "$title<br>";
+echo "Tu", "funkcja", "echo<br>";
+echo ("Tu_funkcja_echo<br>");
+print "Zgłasza".  "się".  "funkcja".  "print<br>";
+print ("Zgłasza się funkcja print<br>")
+/*
+ Kita to kox
+ Walas nie kox
+ */
+?>
+<?="Tak<br>"?>
+<?="I". "śmierdzi". "od". "niego"?>
+
+<li><a href="index.php">Powrót do strony głównej</a></li>
+
+</body>
+</html>
