67c9819dadcfffc511580ffb061554068b858f37
dt19kukulskis <skukulski1@gmail.com>
2022-09-28 09:33:07 +0200

    Add plan_lekcji.php

---
+++ plan_lekcji.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+
+<!-- Add your site or application content here -->
+<h1>Jakub Strycharczyk</h1>
+<script src="js/vendor/modernizr-3.11.2.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async></script>
+
+<table>
+    <caption>PLAN LEKCJI</caption>
+    <thead>
+    <tr>
+        <td>Godz</td> <td>Poniedziałek</td> <td>Wtorek</td> <td>Sroda</td> <td>Czwartek</td> <td>Piatek</td>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>8<sup><u>00</u></sup>-8<sup><u>45</u></sup></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>8<sup><u>50</u></sup>-9<sup><u>35</u></sup></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>9<sup><u>40</u></sup>-10<sup><u>25</u></sup></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+
+    </tr>
+    <tr>
+        <th colspan="6">Zespół Szkół Informatycznych</th>
+    </tr>
+    <style>
+        th, td {
+            border: 1px solid black;
+        }
+        table {
+            border-collapse: collapse;
+            text-align: center;
+            width: 100%;
+            table-layout: fixed;
+        }
+        h1{
+            text-align: center;
+        }
+    </style>
+    </tbody>
+</table>
+
+<li><a href="index.php">Powrót do strony głównej</a></li>
+
+</body>
+
+</html>
