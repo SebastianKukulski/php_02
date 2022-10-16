762a231c0bfdc960c76eb7cca3bb6247dd58c0c2
Sebastian <skukulski1@gmail.com>
2022-10-16 15:04:56 +0200

    ADD comments

--- conditionals.php
+++ conditionals.php
@@ -9,15 +9,17 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div style="text-align: center;">
+<div style="text-align: center;"> /*Wyśrodkowuje wszystko w divie*/
+       
     <li><a href="conditionals.php">Refresh</a></li>
+    
 <?php
-$losowa = rand(1,30);
+$losowa = rand(1,30);   /*Polecenie rand tworzy liczbę losowaą z zakresu 1-30   */
 
 ?>
 <h4 style="color: magenta" >   <!Ustala kolor magenta>
     <?php
-if (20 <= $losowa)
+if (20 <= $losowa) /*   Ustala warunek jeśli liczba 20 jest większa lub równa liczbie wylosowanej wypisz to co jest wpisane w echo> */
 echo "Wylosowana liczba jest większa lub równa 20";   
 ?>
 </h4>
@@ -25,7 +27,7 @@ echo "Wylosowana liczba jest większa lub równa 20";
 <h2  style="color: darkorchid" style="text-align: center"><?= $losowa ?> Liczba losowa</h2>
 <h4 style="color: navy">
     <?php
-    if ($losowa%2==0)
+    if ($losowa%2==0)    /* TWorzy warunek jeśli liczba jest parzysta wyświetl to co w echo, a jeśli nie to co w else  */
         echo "Wylosowana liczba jest parzysta";
         else echo "Liczba nie jest parzysta";
 
@@ -37,14 +39,14 @@ echo "Wylosowana liczba jest większa lub równa 20";
 <h4 style="color: deeppink">
 
     <?php
-    if ($losowa<=10)
+    if ($losowa<=10)   /*Jeśli liczba jest większa lub równa 10 wyświetl to co w echo   */
 
         echo "Jedna dziesiątka";
 
-    elseif ($losowa<=20)
+    elseif ($losowa<=20) /*Jeśli liczba jest większa lub równa 20 wyświetl to co w echo   */
         echo "Dwie dziesiątki";
 
-    elseif ($losowa<=30)
+    elseif ($losowa<=30)  /*Jeśli liczba jest większa lub równa 30 wyświetl to co w echo   */
         echo "Trzy dziesiątki";
 
     ?>
@@ -54,11 +56,11 @@ echo "Wylosowana liczba jest większa lub równa 20";
 </h4>
 
     <?php
-    $miesiac = rand(1,15);
+    $miesiac = rand(1,15);   /*Polecenie rand tworzy liczbę losową z zakresu 1-15   */
 
 
     ?>
-    <h1 style="color: red; border-style: dotted;
+    <h1 style="color: red; border-style: dotted; 
   border-width: 4px;">
     <?php
     echo $miesiac;
@@ -70,29 +72,29 @@ echo "Wylosowana liczba jest większa lub równa 20";
   color: pink;
   font-family: cursive">
     <?php
-    switch ($miesiac){
-        case 1:
+    switch ($miesiac){     
+        case 1:   /*Jeśli wylosowana liczba jest pomiędzy 1-3 wyświetl to co w echo poniżej   */
         case 2:
         case 3:
             echo "Pierwszy kwartał";
-            break;
-        case 4:
+            break;   /*Jeśli to nie jest liczba 1-3 przejdz do następnego  */
+        case 4:  /*Jeśli wylosowana liczba jest pomiędzy 4-6 wyświetl to co w echo poniżej   */
         case 5:
         case 6:
             echo "Drugi kwartał";
-            break;
-        case 7:
+            break;/*Jeśli to nie jest liczba 4-6 przejdz do następnego  */
+        case 7: /*Jeśli wylosowana liczba jest pomiędzy 7-9 wyświetl to co w echo poniżej   */
         case 8:
         case 9:
             echo "Trzeci kwartał";
-            break;
-        case 10:
+            break;/*Jeśli to nie jest liczba 7-9 przejdz do następnego  */
+        case 10:  /*Jeśli wylosowana liczba jest pomiędzy 10-12 wyświetl to co w echo poniżej   */
         case 11:
         case 12:
             echo "Czwarty kwartał";
-            break;
-        default:
-            echo "Błędny numer miesiąca";
+            break;/*Jeśli to nie jest liczba 10-12 przejdz do następnego  */
+        default:/*Jeśli to nie jest liczba żadna z tych powyżej wyświetl to co w echo   */
+            echo "Błędny numer miesiąca";  
     }
 
     ?>
