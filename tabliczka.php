72c4a226bb4672e23da39bd4b4c33bf3ded1776c
dt19kukulskis <skukulski1@gmail.com>
2022-12-14 10:16:54 +0100

    .

---
+++ tabliczka.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            border: 1px solid black;
+        }
+
+        td, tr {
+            border: 1px dotted black;
+            border-collapse: collapse;
+        }
+
+        tr {
+            background-color: rgba(80, 36, 36, 0.52);
+            font-family: Arial;
+            font-size: large;
+        }
+
+    </style>
+</head>
+<body>
+<div style="text-align: center;">
+    <table>
+        <h3>Tabliczka mnożenia</h3>
+        <?php
+        $poziome = 10;
+        $pionowe = 10;
+
+        for ($i = 0; $i <= $pionowe; $i++) {
+            echo '<tr>';
+            for ($j = 0; $j <= $poziome; $j++) {
+                if (($i == 0) && ($j == 0)) echo '<td style="background: rgba(80,36,36,0.52);">x</td>';
+                elseif ($i == 0) echo '<td style="background: rgba(80,36,36,0.52);">' . $j . '</td>';
+                elseif ($j == 0) echo '<td style="background: rgba(80,36,36,0.52);">' . $i . '</td>';
+                else echo '<td>' . $i * $j . '</td>';
+            }
+            echo '</tr>';
+        }
+        ?>
+
+    </table>
+
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
